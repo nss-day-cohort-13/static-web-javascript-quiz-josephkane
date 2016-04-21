d2407b3641b65abca1f5e19321f36312feb1f72f
Joseph Kane <josephckane@gmail.com>
2016-04-21 09:35:41 -0500

    add comments so I understand this code next week

--- quiz.js
+++ quiz.js
@@ -50,46 +50,47 @@
 
 
 
-var heightBox = document.getElementById("treeHeight");
-heightBox.addEventListener("keypress", enter);
+var heightBox = document.getElementById("treeHeight");		// target input box in HTML, make into a variable
+heightBox.addEventListener("keypress", enter);						// add an event listener that triggers
+																													// the function "enter" upon a keypress
 
 var charBox = document.getElementById("character");
 charBox.addEventListener("keypress", enter);
 
+var button = document.getElementById("submitButton");		// target the "grow" button and assign it to a variable
+button.addEventListener("click", middle);								// create an event listener that, upon the click of the button variable, runs the function "middle"
 
 
 
-function enter (e) {
-	var key = e.keyCode;
+function enter (e) {															// function that says "if the keyCode of the key thats pressed is 13,
+	var key = e.keyCode;														// run the function 'middle'
 	if (13 === key)	{
 		middle();
 	}
 }
 
-var treeObject = {};
+var treeObject = {};															// create an empty object called "treeObject"
 
 function middle () {
-	treeObject.height = heightBox.value;
-	treeObject.character = charBox.value;
-	buildTree(treeObject)
+	treeObject.height = heightBox.value;						// create a key in treeObject called "height" and add to it the text in the height input box
+	treeObject.character = charBox.value;						// create a key in treeObject called "character" and add to it the text in the character input box
+	buildTree(treeObject)														// run the function "buildTree" with the treeObject object
 	console.log("height:", treeObject.height);
 	console.log("character", treeObject.character);
 }
 
-var button = document.getElementById("submitButton");
-button.addEventListener("click", middle);
 
 function buildTree (x) {
 
-	var tree = "";
+	var tree = "";															// create a variable "tree" that contains an empty string
 
 
-	if (x.height === "" || x.character === "") {
-		alert("Oops! I'm missing information, Please fill out the WHOLE form.");
-	}
+	if (x.height === "" || x.character === "") {																		// if the value of the key "height" is empty OR
+		alert("Oops! I'm missing information, Please fill out the WHOLE form.");			// if the value of the key "character" is empty
+	}																																								// when this function is called, display this alert
 
-	for (var i = 0; i < x.height; i++) {
-		tree += " ".repeat(x.height - i - 1) + x.character.repeat((2 * i) + 1) + '\n';
+	for (var i = 0; i < x.height; i++) {																							// use "i" to increment/decrement the key values
+		tree += " ".repeat(x.height - i - 1) + x.character.repeat((2 * i) + 1) + '\n';	// based on the index of the for loop
 	}
 
 		console.log(tree);
